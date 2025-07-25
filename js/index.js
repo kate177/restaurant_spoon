35d87dab1cdffbda2213edeff4c72568c801a903
kate177 <70916726+kate177@users.noreply.github.com>
2025-07-25 13:19:00 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,6 +4,56 @@ document.querySelector(".burger").addEventListener("click", function () {
   document.body.classList.toggle("no-scroll");
 });
 
+document.querySelector(".signIn").addEventListener("click", function () {
+  document.getElementById("signIn").classList.add("open");
+});
+
+document
+  .querySelector(".modal-window__cross")
+  .addEventListener("click", function () {
+    document.getElementById("signIn").classList.remove("open");
+  });
+
+const overlay = document.querySelector(".modal-overlay");
+// const modal = document.querySelector(".modal-window");
+
+overlay.addEventListener("click", (e) => {
+  if (e.target === overlay) {
+    overlay.classList.remove("open");
+  }
+});
+
+document
+  .querySelector(".menu__link--frame")
+  .addEventListener("click", function () {
+    document.getElementById("logIn").classList.add("open");
+  });
+
+document.querySelector(".intro__button").addEventListener("click", function () {
+  document.getElementById("logIn").classList.add("open");
+});
+document.querySelector(".promo__link").addEventListener("click", function () {
+  document.getElementById("logIn").classList.add("open");
+});
+document
+  .querySelector(".modal-window__crossLg")
+  .addEventListener("click", function () {
+    document.getElementById("logIn").classList.remove("open");
+  });
+const overlayLg = document.querySelector(".modal-overlayLg");
+overlayLg.addEventListener("click", (e) => {
+  if (e.target === overlayLg) {
+    overlayLg.classList.remove("open");
+  }
+});
+
+const checkbox = document.querySelector(".modal-window__checkbox");
+const button = document.querySelector(".modal-window__btnLi");
+
+checkbox.addEventListener("change", () => {
+  button.disabled = !checkbox.checked;
+});
+///////////////////////////////////////////////////////////////
 const boxes = document.querySelectorAll(".start-animation");
 
 const observer = new IntersectionObserver(
